b2ddfa8c9b05d5682e91a6f172acd282c3805b87
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-02-01 14:00:06 +0000

    Correction

--- src/app/utils/counter.test.ts
+++ src/app/utils/counter.test.ts
@@ -1,12 +1,12 @@
 import { decrementCounter, incrementCounter } from "./counter";
 
-describe(incrementCounter, () => {
+describe("incrementCounter", () => {
   it("should increment by 1", () => {
     expect(incrementCounter(2)).toEqual(3);
   });
 });
 
-describe(decrementCounter, () => {
+describe("decrementCounter", () => {
   it("should decrement by 1", () => {
     expect(decrementCounter(20)).toEqual(19);
   });
